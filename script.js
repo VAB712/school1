f25bb1139dd5bdf663bfcfa0adfa15170549e1ad
VAB712 <114145360+VAB712@users.noreply.github.com>
2023-09-28 23:38:09 +0530

    uploaded all

---
+++ script.js
@@ -0,0 +1,47 @@
+const split1 = () => {
+    var n = document.getElementById("num1").value;
+    document.getElementById("d10").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+    document.getElementById("d11").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+    document.getElementById("d12").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+    document.getElementById("d13").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+    document.getElementById("d14").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+    document.getElementById("d15").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+    document.getElementById("d16").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+    document.getElementById("d17").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+    document.getElementById("d18").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+    document.getElementById("d19").innerHTML = parseInt(n % 10);
+    n = parseInt(n / 10);
+}
+
+const split2 = () => {
+    var N = document.getElementById("num2").value;
+    document.getElementById("d20").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+    document.getElementById("d21").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+    document.getElementById("d22").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+    document.getElementById("d23").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+    document.getElementById("d24").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+    document.getElementById("d25").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+    document.getElementById("d26").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+    document.getElementById("d27").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+    document.getElementById("d28").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+    document.getElementById("d29").innerHTML = parseInt(N % 10);
+    N = parseInt(N / 10);
+}
